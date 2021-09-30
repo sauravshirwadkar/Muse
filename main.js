fd5d658170b0f474c5476c7bc8dc1ab5463617aa
Vajresh <vajresh005@example.com>
2021-09-30 13:35:14 +0530

    Enhancement: Added removeItem
    removeItemFromList function is added
    It reomves the item if the quantity is less than 1
    
    Because:
    - Item should not appear in cart with 0 quantity
    
    If all items are removed then clearCart() is called
    
    Author: VAJRESH

--- main.js
+++ main.js
@@ -155,6 +155,25 @@ function setItems(product){
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
+function removeItemFromList(product){ 
+    // load existing data
+    let cartItems = localStorage.getItem('productsInCart');
+    
+    cartItems = JSON.parse(cartItems); 
+    delete cartItems[product.tag]
+    if (Object.keys(cartItems).length === 0) return clearCart();
+
+    // loop through items and calculate total price
+    let totalCost = 0;
+    Object.values(cartItems).forEach((item) => {
+        totalCost += item.price * item.inCart;
+    })
+    
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+
+    localStorage.setItem("totalCost", totalCost);
+}
+
 function updateQuantity(product, quantity=1){ 
     // load existing data
     let cartItems = localStorage.getItem('productsInCart');
@@ -164,7 +183,7 @@ function updateQuantity(product, quantity=1){
     cartItems[product.tag].inCart += quantity;
 
     const totalQuantity = +cartItems[product.tag].inCart
-    if (totalQuantity < 1) return;
+    if (totalQuantity < 1) return removeItemFromList(product);
 
     // loop through items and calculate total price
     let totalCost = 0;
